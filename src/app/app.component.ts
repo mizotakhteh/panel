4fbcba41e41a424b6ad1da074c6c14b82142513a
Hasan Karimi <hasankarimi.dev@gmail.com>
2023-04-24 17:55:09 +0330

    feat: base ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MediaObserver } from "@angular/flex-layout";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'panel';
+
+  constructor(public mediaObserver: MediaObserver) { }
 }
